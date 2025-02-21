0489f5eb1c0a1a11f8da61804e0b49c7032eff77
Vergueirod <diego.ssv@outlook.com>
2025-02-21 07:10:32 -0300

    Created test for validation with all values are right

--- test_category.py
+++ test_category.py
@@ -1,15 +1,19 @@
 import unittest
 from category import Category
+from uuid import UUID
 
 class TestCategory(unittest.TestCase):
     def test_name_is_required(self):
         with self.assertRaisesRegex(TypeError, "missing 1 required positional argument: 'name'"):
             Category()
 
-    def test_name_is_required(self):
+    def test_name_must_have_less_than_255_characters(self):
         with self.assertRaisesRegex(ValueError, "name must have less than 256 characters"):
             Category(name="a" * 256)
     
+    def test_category_must_be_created_with_id_as_uuid(self):
+        category = Category(name="Move")
+        self.assertEqual(type(category.id), UUID)
 
 if __name__ == "__main__":
     unittest.main()
