552d9c99b2c28284e8a10cab76bd58aa7ab5a591
Vergueirod <diego.ssv@outlook.com>
2025-02-23 11:10:19 -0300

    handling exceptions

--- src/core/category/tests/application/test_create_category.py
+++ src/core/category/tests/application/test_create_category.py
@@ -1,5 +1,6 @@
+import pytest
 from uuid import UUID
-from src.core.category.application.create_category import create_category
+from src.core.category.application.create_category import create_category, InvalidCategoryData
 
 class TestCreateCategory:
     def test_create_category_with_calid_data(self):
@@ -11,3 +12,12 @@ class TestCreateCategory:
     
         assert category_id is not None
         assert isinstance(category_id, UUID)
+
+    def teste_create_category_with_invalid_data(self):
+        with pytest.raises(InvalidCategoryData, match="name cannot be empty") as exc_info:
+            category_id = create_category(
+                name="",
+                )
+
+        assert exc_info.type is InvalidCategoryData
+        assert str(exc_info.value) == "name cannot be empty"
