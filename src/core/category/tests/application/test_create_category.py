54b1a81d9143a802e434cef4c29169be3dea5820
Vergueirod <diego.ssv@outlook.com>
2025-02-23 11:26:49 -0300

    using in memory repository

--- src/core/category/tests/application/test_create_category.py
+++ src/core/category/tests/application/test_create_category.py
@@ -1,10 +1,16 @@
 import pytest
+from unittest.mock import MagicMock
 from uuid import UUID
 from src.core.category.application.create_category import create_category, InvalidCategoryData
+from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
+
 
 class TestCreateCategory:
     def test_create_category_with_calid_data(self):
+        mock_repository = MagicMock(InMemoryCategoryRepository)
+
         category_id = create_category(
+            repository=mock_repository,
             name="Moves",
             description="All moves",
             is_active=True,
@@ -12,6 +18,7 @@ class TestCreateCategory:
     
         assert category_id is not None
         assert isinstance(category_id, UUID)
+        assert mock_repository.save.called is True
 
     def teste_create_category_with_invalid_data(self):
         with pytest.raises(InvalidCategoryData, match="name cannot be empty") as exc_info:
