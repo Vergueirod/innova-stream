1295cbd4dc466020a012898440e329f084ef06da
Vergueirod <diego.ssv@outlook.com>
2025-02-25 08:33:32 -0300

    test_when_category_does_not_exist_then_raise_exception

--- src/core/category/tests/integration/test_get_category.py
+++ src/core/category/tests/integration/test_get_category.py
@@ -1,7 +1,10 @@
-from uuid import UUID
+import uuid
+import pytest
+from uuid import UUID, uuid4
 from src.core.category.application.get_category import GetCategory, GetCategoryRequest, GetCategoryResponse
 from src.core.category.domain.category import Category
 from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
+from src.core.category.application.exceptions import CategoryNotFound
 
 
 class TestGetCategory:
@@ -33,3 +36,25 @@ class TestGetCategory:
             description="Categoria para filmes",
             is_active=True,
         )
+
+        def test_when_category_does_not_exist_then_raise_exception(self):
+            category_filme = Category(
+                name="Filme",
+                description="Categoria para filmes",
+                is_active=True,
+            )
+            category_serie = Category(
+                name="Serie",
+                description="Categoria por serie",
+                is_active=True,
+            )
+            repository = InMemoryCategoryRepository(
+                categories=[category_filme, category_serie]
+            )  
+            use_case = GetCategory(repository=repository)
+            request = GetCategoryRequest(
+                id=uuid.uuid4()
+            )
+
+            with pytest.raises(CategoryNotFound) as exc:
+                use_case.execute(request)
